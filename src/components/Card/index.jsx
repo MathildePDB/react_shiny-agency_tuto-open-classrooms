ce26ffe2014acd1aef632d6ffd77bbe2931a338f
MathildePDB <mathildepdb@laposte.net>
2023-09-15 15:19:19 +0200

    add global styles

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,26 +1,41 @@
 import PropTypes from 'prop-types'
 import DefaultPicture from '../../assets/profile.png'
 import styled from 'styled-components'
+import colors from '../../style/colors'
 
 const CardLabel = styled.span`
-    color: #5843e4;
-    font-size: 22px;
-    font-weight: bold;
-  `
-  const CardImage = styled.img`
-    height: 80px;
-    width: 80px;
-    border-radius: 50%;
-  `
+  color: #5843e4;
+  font-size: 22px;
+  font-weight: bold;
+`
+const CardImage = styled.img`
+  height: 80px;
+  width: 80px;
+  border-radius: 50%;
+`
+
+const CardWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 15px;
+  background-color: ${colors.backgroundLight};
+  border-radius: 30px;
+  width: 350px;
+  transition: 200ms;
+  &:hover {
+    cursor: pointer;
+    box-shadow: 2px 2px 10px #E2E2E9;
+  }
+`
 
 function Card({label, picture, title}) {
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', padding: 15 }}>
+    <CardWrapper>
       <CardLabel>{label}</CardLabel>
       <CardImage src={picture} alt="freelance" />
       <span>{title}</span>
-    </div>
+    </CardWrapper>
   )
 }
 
