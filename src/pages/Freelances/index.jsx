0ba04632d006b851aacf8fbe1981da56712374fb
MathildePDB <mathildepdb@laposte.net>
2023-09-15 10:53:30 +0200

    add prop-types to cards

--- src/pages/Freelances/index.jsx
+++ src/pages/Freelances/index.jsx
@@ -1,7 +1,36 @@
+import DefaultPicture from '../../assets/profile.png'
+import Card from '../../components/Card'
+
+const freelanceProfiles = [
+  {
+    name: 'Jane Doe',
+    jobTitle: 'DevOps',
+    picture: DefaultPicture,
+  },
+  {
+    name: 'John Doe',
+    jobTitle: 'Développeur frontend',
+    picture: DefaultPicture,
+  },
+  {
+    name: 'Jeanne Biche',
+    jobTitle: 'Développeuse fullstack',
+    picture: DefaultPicture,
+  }
+]
+
 function Freelances() {
   return (
     <div>
-      <h1>Page des freelances</h1>
+      <h1>Freelances 👩‍💻👨‍💻👩‍💻</h1>
+      {freelanceProfiles.map((profile, index) => (
+        <Card
+          key={`${profile.name}-${index}`}
+          label={profile.jobTitle}
+          picture={profile.picture}
+          title={profile.name}
+        />
+      ))}
     </div>
   )
 }
