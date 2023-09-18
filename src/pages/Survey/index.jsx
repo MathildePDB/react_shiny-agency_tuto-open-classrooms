0f61d0288df2eb79b590ba3e69426245c23733ab
MathildePDB <mathildepdb@laposte.net>
2023-09-18 17:25:00 +0200

    add Context to survey results

--- src/pages/Survey/index.jsx
+++ src/pages/Survey/index.jsx
@@ -1,9 +1,10 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useContext } from 'react'
 import { useParams } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 import colors from '../../utils/style/colors'
 import { Loader } from '../../utils/style/Atoms'
+import { SurveyContext } from '../../utils/context'
 
 const SurveyContainer = styled.div`
   display: flex;
@@ -29,6 +30,30 @@ const LinkWrapper = styled.div`
     margin-right: 20px;
   }
 `
+const ReplyBox = styled.button`
+  border: none;
+  height: 100px;
+  width: 300px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${colors.backgroundLight};
+  border-radius: 30px;
+  cursor: pointer;
+  bos-shadow: ${(props) =>
+    props.isSelected ? `0px 0px 0px 2px ${colors.primary} inset` : 'none'};
+  &:first-child {
+    margin-right: 15px;
+  }
+  &:last-of-type {
+    margin-left: 15px;
+  }
+`
+
+const ReplyWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+`
 
 function Survey() {
   const { questionNumber } = useParams()
@@ -37,6 +62,7 @@ function Survey() {
   const nextQuestionNumber = questionNumberInt + 1
   const [surveyData, setSurveyData] = useState({})
   const [isDataLoading, setDataLoading] = useState(false)
+  const { saveAnswers, answers } = useContext(SurveyContext)
   const [error, setError] = useState(false)
 
 // Cette synthaxe permet aussi bien de faire des calls API.
@@ -55,6 +81,10 @@ function Survey() {
 // setError(true)
 //  }
 // }
+
+  function saveReply(answer) {
+    saveAnswers({ [questionNumber]: answer })
+  }  
 
   useEffect(() => {
     async function fetchSurvey() {
@@ -85,6 +115,20 @@ function Survey() {
       ) : (
         <QuestionContent>{surveyData[questionNumber]}</QuestionContent>
       )}
+      <ReplyWrapper>
+        <ReplyBox
+          onClick={() => saveReply(true)}
+          isSelected={answers[questionNumber] === true}
+        >
+          Oui
+        </ReplyBox>
+        <ReplyBox
+          onClick={() => saveReply(false)}
+          isSelected={answers[questionNumber] === false}
+        >
+          Non
+        </ReplyBox>
+      </ReplyWrapper>
       <LinkWrapper>
         <Link to={`/survey/${prevQuestionNumber}`}>Précédent</Link>
         {surveyData[questionNumberInt + 1] ? (
