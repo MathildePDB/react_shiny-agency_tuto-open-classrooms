160910e9efe86121494b13b5103244e6556ea9c1
MathildePDB <mathildepdb@laposte.net>
2023-09-18 16:36:01 +0200

    add dark mode with context

--- src/index.jsx
+++ src/index.jsx
@@ -1,37 +1,34 @@
 import React from 'react'
 import reactdom from 'react-dom'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Home from './pages/Home/'
 import Survey from './pages/Survey'
 import Header from './components/Header'
+import Footer from './components/Footer'
 import Error from './components/Error'
 import Results from './pages/Results'
 import Freelances from './pages/Freelances'
-import { createGlobalStyle } from 'styled-components'
-
-const GlobalStyle = createGlobalStyle`
-  * {
-    font-family: 'Trebuchet MS', Helvetica, sans-serif;
-  }
-
-  body {
-    margin: 0;
-  }
-`
+import GlobalStyle from './utils/style/GlobalStyle'
+import { ThemeProvider, SurveyProvider } from './utils/context'
 
 reactdom.render(
   <React.StrictMode>
     <Router>
-      <GlobalStyle />
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/survey/:questionNumber" element={<Survey />} />
-        <Route path="/results" element={<Results />} />
-        <Route path="/freelances" element={<Freelances />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
+      <ThemeProvider>
+        <SurveyProvider>
+          <GlobalStyle />
+          <Header />
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/survey/:questionNumber" element={<Survey />} />
+            <Route path="/results" element={<Results />} />
+            <Route path="/freelances" element={<Freelances />} />
+            <Route path="*" element={<Error />} />        
+          </Routes>
+          <Footer />
+        </SurveyProvider>
+      </ThemeProvider>
     </Router>
   </React.StrictMode>,
-  document.getElementById('root'),
-)
+  document.getElementById('root')
+);
---
+++ src/utils/context/index.jsx
@@ -0,0 +1,31 @@
+import React, { useState, createContext } from 'react'
+
+export const ThemeContext = createContext()
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState('light')
+  const toggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light')
+  }
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  )
+}
+
+export const SurveyContext = createContext()
+
+export const SurveyProvider = ({ children }) => {
+  const [answers, setAnswer] = useState({})
+  const saveAnswers = (newAnswers) => {
+    setAnswer({ ...answers, ...newAnswers })
+  }
+
+  return (
+    <SurveyContext.Provider value={{ answers, saveAnswers }}>
+      {children}
+    </SurveyContext.Provider>
+  )
+}
